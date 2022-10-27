99497b216a61f886ac85840593aeecc28414ee62
agaipova <m.mr.74@mail.ru>
2022-10-27 11:19:41 +0300

    Версия 1

---
+++ GaussProgram/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace GaussProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(9, 54);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(543, 341);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(9, 28);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(145, 28);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(281, 28);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 3;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Кол-во точек";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "μ (Му)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "σ (Сигма)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(423, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 20);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Сгенерировать точки";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(558, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(161, 381);
+            this.listBox1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(710, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Проверить по критерию Пирсона";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 434);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.chart1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Метод Гаусса для генерации точек нормального распределения";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ GaussProgram/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GaussProgram
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        public Form1()
+        {
+            //В этом методе реализация всей формы (график, список, кнопки и другие элементы управления)
+            InitializeComponent();
+        }
+
+        //Таблица сгенерированных точек (точки X и их количество Y)
+        Hashtable points;
+
+        /// <summary>
+        /// Метод, который срабатывает по нажатию на кнопку "Сгенерировать точки"
+        /// </summary>
+        /// <param name="sender">Какой элемент вызвал метод</param>
+        /// <param name="e">Дополнительные параметры</param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //очистка списка от точек
+            listBox1.Items.Clear();
+            //очистка графика от точек
+            chart1.Series[0].Points.Clear();
+
+            //вызываем генерацию чисел методом Гаусса (количество чисел, параметр "му", параметр "сигма")
+            //получаем все точки в массив mass
+            int[] mass = GausseMethod(Convert.ToInt32(numericUpDown1.Value), Convert.ToInt32(numericUpDown2.Value), Convert.ToInt32(numericUpDown3.Value));
+
+            //формируем таблицу X и Y из массива mass
+            points = RandomFrequency(mass);
+            //добавляем X и Y в список через пробел и на график
+            foreach (DictionaryEntry item in points)
+            {
+                chart1.Series[0].Points.AddXY(item.Key, item.Value);
+                listBox1.Items.Add(item.Key.ToString()+ "  "+ item.Value.ToString());
+            }
+        }
+
+
+        /// <summary>
+        /// Получение массива случайных целых чисел с помощью метода Гаусса
+        /// </summary>
+        /// <param name="num">Количество необходимых элементов</param>
+        /// <param name="mu">μ (Му) параметр</param>
+        /// <param name="sigma">σ(Сигма) параметр</param>
+        /// <returns>Полученный массив целых чисел</returns>
+        public int[] GausseMethod(int num, double mu, double sigma)
+        {
+            //массив целых чисел (сейчас все числа = 0)
+            int[] res = new int[num];
+
+            //Генератор
+            Random rand = new Random();
+            for (int i = 0; i < num; i++)
+            {
+                //Сюда будет накапливать сумму
+                double summ = 0;
+                //Выполняем 13 интераций накапливания суммы со случайных чисел
+                for (int j = 0; j < 13; j++)
+                {
+                    //получение случайного вещественного числа
+                    double number = rand.NextDouble();
+                    //накапливаем сумму
+                    summ = summ + number;
+                }
+                //вычисляем итоговой число по формуле (оставляем три знака после запятой)
+                double dRandValue = Math.Round(mu + sigma * (summ - 6), 3);
+                //записываем результат в массив в виде целого числа
+                res[i] = (int) Math.Round(dRandValue);
+            }
+
+            //возвращаем массив
+            return res;
+
+        }
+
+        /// <summary>
+        /// Функция для формирования X и Y из массива randomNums
+        /// </summary>
+        /// <param name="randomNums">Массив случайных целых чисел</param>
+        /// <returns></returns>
+        private Hashtable RandomFrequency(int[] randomNums)
+        {
+            Hashtable HT = new Hashtable();
+            int N = randomNums.Length;
+
+            for (int i = 0; i <= N - 1; i++)
+            {
+                if (HT.ContainsKey(randomNums[i]))
+                    HT[randomNums[i]] = (int)HT[randomNums[i]] + 1;
+                else
+                    HT[randomNums[i]] = 1;
+            }
+
+            return HT;
+        }
+
+        /// <summary>
+        /// Метод проверки криетрия Пирсона
+        /// </summary>
+        /// <param name="HT">X и Y</param>
+        public void Pirson(Hashtable HT)
+        {
+            //будем записывать отдельно X и Y
+            double[] X = new double[HT.Count];
+            double[] Y = new double[HT.Count];
+            int j = 0;
+            //получение X и Y отдельно из HT
+            foreach (DictionaryEntry item in HT)
+            {
+                X[j] = (int)item.Key;
+                Y[j] = (double) ((Convert.ToDouble(item.Value)) / ((double) HT.Count));
+                j++;
+            }
+
+            //вычисление среднего арифметического X
+            double XAvg = X.Average();
+            //вычисление среднего арифметического Y
+            double YAvg = Y.Average();
+
+            //вычисление чисел dx по формуле
+            double[] dX = new double[HT.Count];
+            for (int i = 0; i < X.Length; i++)
+                dX[i] = X[i] - XAvg;
+
+            //вычисление чисел dy по формуле
+            double[] dY = new double[HT.Count];
+            for (int i = 0; i < Y.Length; i++)
+                dY[i] = Y[i] - YAvg;
+
+            //вычисление чисел dx в квадрате
+            double[] dX2 = new double[HT.Count];
+            for (int i=0;i<X.Length; i++)
+                dX2[i] = Math.Pow(dX[i], 2);
+
+            //вычисление чисел dy в квадрате
+            double[] dY2 = new double[HT.Count];
+            for (int i = 0; i < Y.Length; i++)
+                dY2[i] = Math.Pow(dY[i], 2);
+
+            //вычисление чисел dx * dy
+            double[] dXMultdY = new double[HT.Count];
+            for (int i = 0; i < X.Length; i++)
+                dXMultdY[i] = dX[i] * dY[i];
+
+            //получение суммы всех чисел dx в квадрате
+            double dX2Sum = dX2.Sum();
+            //получение суммы всех чисел dy в квадрате
+            double dY2Sum = dY2.Sum();
+            //получение суммы всех чисел dx * dy
+            double dXMultdYSum = dXMultdY.Sum();
+
+            //количество степеней свободы = количество X - 2
+            int f = HT.Count - 2;
+
+            //расчёт коэффициента корреляции rXY
+            double rXY = dXMultdYSum / (Math.Sqrt(dX2Sum * dY2Sum));
+            //расчёт t-критерия
+            double t = rXY * Math.Sqrt(f) / (Math.Sqrt(1 - Math.Pow(rXY, 2)));
+
+            //вывод полученных данных на экран
+            MessageBox.Show("Количество степеней свободы (f): " + f + Environment.NewLine+"t-критерий (расчитанный): "+t + Environment.NewLine + "Значение коэффициента корреляции (rXY): " + rXY);
+        }
+
+        /// <summary>
+        /// Метод, который срабатывает по нажатию на кнопку "ПРоверить по критерию Пирсона"
+        /// </summary>
+        /// <param name="sender">Какой элемент вызвал метод</param>
+        /// <param name="e">Дополнительные параметры</param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Pirson(points);
+        }
+    }
+}
